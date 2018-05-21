59204166fb7af0a1facc00e2c2d1e635927e61bd
BuildTools <unconfigured@null.spigotmc.org>
2018-05-21 18:45:53 +0800

    start a new plugin more functions

---
+++ src/com/xuchaoji/craft/easygiftpack/EasyGiftPack.java
@@ -0,0 +1,17 @@
+package com.xuchaoji.craft.easygiftpack;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EasyGiftPack extends JavaPlugin {
+	//enable
+	@Override
+	public void onEnable() {
+		
+	}
+	//disable
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
